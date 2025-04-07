aab2bbf6cce9cc7de9c981fe67580c21695917dd
mio1432 <mio.gullbrand@gmail.com>
2025-04-07 14:32:38 +0200

    css

--- index.php
+++ index.php
@@ -13,30 +13,19 @@ if(isset($_POST["submit"])){
     
 }
 
+
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>scaniaboss</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>jag är scaniaboss</h1>
-
-<?php 
-$sql="SELECT * FROM tbluser";
-$result=mysqli_query($conn, $sql);
-while($rad=mysqli_fetch_assoc($result)){ ?>
+    <div class="header"><h1>chatt</h1></div>
 
 
-  <p>
-    <b>Användarnamn:</b>&nbsp;<?=$rad["username"]?><br>
-    <b>Lösenord:</b>&nbsp;<?=$rad["password"]?>
-</p>
-  
-
-<?php }
-?>
 <form action="index.php" method="POST">
     <input type="text" name="username" placeholder="Användarnamn">
     <input type="password" name="password" placeholder="Lösenord">
